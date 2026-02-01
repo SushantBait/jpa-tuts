53deb5c1291db466361cbdd9f71c5a50d78cea8f
Sushant Bait <sushantbait93@gmail.com>
2026-02-01 12:16:43 +0530

    JPA Tuts with MySql DB connection with table creation

---
+++ src/test/java/com/sushant/jpatutorial/jpa/tuts/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushant.jpatutorial.jpa.tuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
