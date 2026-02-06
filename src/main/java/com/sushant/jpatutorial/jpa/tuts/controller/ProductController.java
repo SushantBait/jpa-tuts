5d1c98f623a8cfe0c955776c89ef4254ef9e5474
Sushant Bait <sushantbait93@gmail.com>
2026-02-06 12:52:46 +0530

    code added for Sorting and pagination

--- src/main/java/com/sushant/jpatutorial/jpa/tuts/controller/ProductController.java
+++ src/main/java/com/sushant/jpatutorial/jpa/tuts/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.sushant.jpatutorial.jpa.tuts.controller;
 
 import com.sushant.jpatutorial.jpa.tuts.dto.ProductDto;
+import com.sushant.jpatutorial.jpa.tuts.entites.ProductEntity;
 import com.sushant.jpatutorial.jpa.tuts.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +24,35 @@ public class ProductController {
 
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<ProductDto>> getProduct(){
+    public ResponseEntity<List<ProductDto>> getProduct() {
         //When we are mark @RequestParam it's an optional data but need to do with @RequestParam(required = false)
 //        return ResponseEntity.ok(productService.getProductByTitleAndOrderByPrice());
         return ResponseEntity.ok(productService.getAllProductOrderByPrice());
+    }
+
+
+    @GetMapping
+    public List<ProductEntity> getAllProducts(
+            @RequestParam(defaultValue = "") String title,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "0") Integer pageNumber) {
+
+        return productService.findByTitleContainingIgnoreCase(
+                title, sortBy, pageNumber);
+
+//
+//        return productRepository.findBy(Sort.by(Sort.Direction.DESC, sortBy, "price"));
+//        return productRepository.findBy(Sort.by(
+//                Sort.Order.desc(sortBy),
+//                Sort.Order.desc("title")
+//        ));
+
+
+//        Pageable pageable = PageRequest.of(
+//                pageNumber,
+//                PAGE_SIZE,
+//                Sort.by(sortBy));
+//
+//        return productRepository.findAll(pageable).getContent();
     }
 }
